9ee3944bd3f595f5d2747f88f717ebe601329945
Avital-Attiah <avitala5515@gmail.com>
2025-12-20 20:21:32 +0200

    Prepare backend for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ import { suggestSubstitutes } from "./controllers/recipe.controller.js";
 import "dotenv/config";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(express.json());
